785f179a7870b0fdc660308fabffb68311ed6a20
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-04-08 16:12:37 -0700

    Added play games code.

--- src/elo.py
+++ src/elo.py
@@ -17,10 +17,69 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+import sys
 import time
+import random
 import chess
 import chess.engine
 
+NUM_GAMES = 64
+RAND_MOVES = 3
+TIME_CONTROL = (90, 0)
+
 
 def test(base_path, new_path):
-    pass
+    score = 0
+
+    for game in range(NUM_GAMES):
+        sys.stdout.write(f"Game {game+1}: ")
+        sys.stdout.flush()
+
+        board = chess.Board()
+        side = random.random() > 0.5
+        if side:
+            white = chess.engine.SimpleEngine.popen_uci(new_path)
+            black = chess.engine.SimpleEngine.popen_uci(base_path)
+        else:
+            white = chess.engine.SimpleEngine.popen_uci(base_path)
+            black = chess.engine.SimpleEngine.popen_uci(new_path)
+        for i in range(RAND_MOVES):
+            board.push(random.choice(list(board.generate_legal_moves())))
+
+        wtime = TIME_CONTROL[0] * 60
+        btime = TIME_CONTROL[0] * 60
+        inc = TIME_CONTROL[1]
+        while True:
+            try:
+                limit = chess.engine.Limit(white_clock=wtime, black_clock=btime, white_inc=inc, black_inc=inc)
+                start = time.time()
+                if board.turn:
+                    board.push(white.play(board, limit).move)
+                else:
+                    board.push(black.play(board, limit).move)
+                elapse = time.time() - start
+                if board.turn:
+                    wtime -= elapse
+                    wtime += inc
+                else:
+                    btime -= elapse
+                    btime += inc
+
+                if board.is_game_over():
+                    result = board.result()
+                    break
+
+            except chess.engine.EngineError:
+                result = "ERROR"
+                break
+
+        print(f"Result is {result}")
+        sys.stdout.flush()
+
+        if side:
+            if result == "1-0":
+                score += 1
+            elif result == "0-1":
+                score -= 1
+
+    return score
