d23c85253f6e69f90648444fd22034d0f4b9f829
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-04-08 15:55:13 -0700

    Base scripts.

---
+++ src/elo.py
@@ -0,0 +1,26 @@
+#
+#  Regression Testing
+#  Scripts to run continuously to test ELO performance quarter-daily.
+#  Copyright Megalodon Chess 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import time
+import chess
+import chess.engine
+
+
+def test(base_path, new_path):
+    pass
